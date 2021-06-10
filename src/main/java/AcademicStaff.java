764fe9e452710e8d45da0f2e102030483c3aee5c
TawfeeqCupido <50589843+TawfeeqCupido@users.noreply.github.com>
2021-06-10 13:18:53 +0200

    Add files via upload

--- src/main/java/AcademicStaff.java
+++ src/main/java/AcademicStaff.java
@@ -1,2 +1,65 @@
 public class AcademicStaff {
+
+    String name;
+    String position;
+    String department;
+    String course;
+
+
+    AcademicStaff(String name, String position, String department, String
+            course) {
+        this.name = name;
+        this.position = position;
+        this.department = department;
+        this.course = course;
+
+
+    }
+
+
+    public String toString() {
+
+        return "Name = " + name + " \n Position = " + position + "\n Department = " + department + "" + "\n Course = " + course + "";
+
+
+    }
+
+
+    public static class AcademicStaffBuilder {
+
+        String name;
+        String position;
+        String department;
+        String course;
+
+        public AcademicStaffBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public AcademicStaffBuilder setPosition(String position) {
+            this.position = position;
+            return this;
+        }
+
+        public AcademicStaffBuilder setDepartment(String department) {
+            this.department = department;
+            return this;
+        }
+
+        public AcademicStaffBuilder setCourse(String course) {
+            this.course = course;
+            return this;
+        }
+
+        public AcademicStaff build() {
+            return new AcademicStaff( name, position, department, course);
+        }
+
+    }
+
+
+
+
 }
+
---
+++ src/main/java/AcademicStaffBuilder.java
@@ -0,0 +1,19 @@
+public class AcademicStaffBuilder extends AcademicStaff {
+
+    AcademicStaffBuilder(String name, String position, String department, String course) {
+        super(name, position, department, course);
+    }
+
+    public static void main (String[] args) {
+
+        AcademicStaff academicstaffobj = new AcademicStaffBuilder()
+                .setName("Tawfeeq Cupido")
+                .setPosition("Intern")
+                .setDepartment("Engineering")
+                .setCourse("Information Technology")
+                .build();
+
+        System.out.println(academicstaffobj);
+    }
+}
+
