cf6511a482ee01cf3616c313a14eb5cc989ca094
Devon Daniels <devon.daniels16@gmail.com>
2021-06-10 13:27:53 +0200

    course

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,2 +1,57 @@
 public class Course {
+    private String courseName, subjectList, department;
+
+    private Course(Builder builder) {
+        this.courseName = builder.courseName;
+        this.subjectList = builder.subjectList;
+        this.department = builder.department;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseName='" + courseName + '\'' +
+                ", subjectList='" + subjectList + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String courseName, subjectList, department;
+
+        public Builder setCourseName(String courseName) {
+            this.courseName = courseName;
+
+            return this;
+        }
+
+        public Builder setSubjectList(String subjectList) {
+            this.subjectList = subjectList;
+
+            return this;
+        }
+
+        public Builder setDepartment(String department) {
+            this.department = department;
+
+            return this;
+        }
+
+
+        public Course Build() {
+
+            return new Course(this);
+        }
+
+        public Builder copy(Course course) {
+            this.courseName = course.courseName;
+            this.subjectList = course.subjectList;
+            this.department = course.department;
+
+            return this;
+        }
+
+
+    }
+
 }
