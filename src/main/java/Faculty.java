47e11d6fa1034c50ef4febf34c8cf9d35f660f6f
MxLegacy <michael.dirks02@gmail.com>
2021-06-11 10:40:51 +0200

    Faculty Entity

--- src/main/java/Faculty.java
+++ src/main/java/Faculty.java
@@ -1,2 +1,42 @@
-public interface Faculty {
+public class Faculty {
+
+    String facultyName = "My Faculty";
+    String[] departmentList = {"System Administration", "Application Development", "Multimedia", "Networking"};
+
+    public Faculty(Faculty.Builder builder) {
+        this.facultyName = builder.facultyName;
+        this.departmentList = builder.departmentList;
+    }
+
+    public static class Builder{
+        private String facultyName;
+        private String[] departmentList;
+
+        public Faculty.Builder setFacultyName(String facultyName){
+            this.facultyName = facultyName;
+
+            return this;
+        }
+
+        public Faculty.Builder setDepartmentyList(String setDepartmentList){
+            this.departmentList = departmentList;
+
+            return this;
+        }
+
+        public Faculty build() {
+            return new Faculty(this);
+        }
+
+        public Faculty.Builder copy(Faculty faculty){
+            this.facultyName = facultyName;
+            this.departmentList = departmentList;
+            return this;
+
+        }
+
+
+    }
+
+
 }
