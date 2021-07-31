5ef8a566b35137f461619104cd46862bd5789d5d
TawfeeqCupido <50589843+TawfeeqCupido@users.noreply.github.com>
2021-07-31 23:34:14 +0200

    Delete ADP-Assignment3-master directory

--- ADP-Assignment3-master/src/main/java/za/ac/cput/Repository/IASRepository.java
+++
@@ -1,8 +0,0 @@
-package za.ac.cput.Repository;
-
-import java.util.Set;
-import za.ac.cput.Entity.AcademicStaff;
-
-public interface IASRepository extends IRepository<AcademicStaff, String>{
-        Set<AcademicStaff> getAll();
-}
