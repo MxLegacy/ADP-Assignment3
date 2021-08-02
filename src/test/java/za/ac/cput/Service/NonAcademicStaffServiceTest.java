7c93c51b7f7622697ea51aaf6d542c7d1f4eb588
Tatum-dw <69960567+Tatum-dw@users.noreply.github.com>
2021-08-02 15:28:02 +0200

    Added Non Academic Staff Service Test

---
+++ src/test/java/za/ac/cput/Service/NonAcademicStaffServiceTest.java
@@ -0,0 +1,43 @@
+package za.ac.cput.Service;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.Entity.NonAcademicStaff;
+import za.ac.cput.factory.NonAcademicStaffFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NonAcademicStaffServiceTest {
+    private static NonAcademicStaffService NASservice = NonAcademicStaffService.getService();
+    private static NonAcademicStaff nonAcademicStaff = NonAcademicStaffFactory.createNonAcademicStaff("Belinda", "Clerk", "Accounting");
+
+    @Test
+    void testCreate(){
+        NonAcademicStaff create = NASservice.create(nonAcademicStaff);
+        assertNotNull(create);
+        System.out.println("Create: " + create);
+    }
+
+    void testRead() {
+        NonAcademicStaff read = NASservice.read(nonAcademicStaff.toString());
+        assertNull(read);
+        System.out.println("Read: " + read);
+    }
+
+    @Test
+    void testUpdate() {
+        NonAcademicStaff update = NASservice.update(nonAcademicStaff);
+        System.out.println("Update: " + update);
+    }
+
+    @Test
+    void testDelete() {
+        boolean success = NASservice.delete(nonAcademicStaff.toString());
+        assertTrue(success);
+        System.out.println("Delete: " + success);
+    }
+
+    @Test
+    void testGetAll() {
+        System.out.println("Get all: " + NASservice.getAll());
+    }
+}
