4021b24706fda3b9a8bb3fc645d43991ddf4bdc0
Tatum-dw <69960567+Tatum-dw@users.noreply.github.com>
2021-08-16 17:39:26 +0200

    Added Main class

---
+++ src/main/java/za/ac/cput/SystemAdministration.java
@@ -0,0 +1,11 @@
+package za.ac.cput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemAdministration {
+    public static void main(String[] args){
+        SpringApplication.run(SystemAdministration.class, args);
+    }
+}
