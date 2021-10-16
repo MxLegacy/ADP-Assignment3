e9fa517b25a1c7d9f6b1354c66aa1f38af1c0c60
Devon Daniels <devon.daniels16@gmail.com>
2021-10-16 18:22:18 +0200

    Course and dept controller done

--- src/test/java/za/ac/cput/Controller/CourseControllerTest.java
+++ src/test/java/za/ac/cput/Controller/CourseControllerTest.java
@@ -4,76 +4,76 @@ package za.ac.cput.Controller;
         Author: Devon Daniels (217299822)
 
  */
-import org.junit.jupiter.api.Test;
+import org.apache.catalina.authenticator.SavedRequest;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import za.ac.cput.Entity.Course;
-import za.ac.cput.factory.CourseFactory;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import za.ac.cput.factory.CourseFactory;
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 
 class CourseControllerTest {
 
-    private static Course course = CourseFactory.buildCourse("first", "ADP", "HR");
+    private static Course course = CourseFactory.buildCourse("MTech: Information Technology ", "  Thesis (R5IT01R)", "Information Technology");
     @Autowired
     private TestRestTemplate restTemplate;
-    private final String baseURL = "http://localhost:8080/course";
+    private final String baseURL= "http://localhost:8080/course";
+
 
     @Test
+
     void create() {
         String url = baseURL + "/create";
         ResponseEntity<Course> postResponse = restTemplate.postForEntity(url, course, Course.class);
         assertNotNull(postResponse);
         assertNotNull(postResponse.getBody());
         course = postResponse.getBody();
-        System.out.println("Course is: " + course);
+        System.out.println("Saved course: " + course);
         assertEquals(course.getCourseName(), postResponse.getBody().getCourseName());
     }
 
     @Test
     void read(){
-        String url = baseURL + "/create";
-        ResponseEntity<Course> postResponse = restTemplate.postForEntity(url, course, Course.class);
-        assertNotNull(postResponse);
-        assertNotNull(postResponse.getBody());
-        course = postResponse.getBody();
-        System.out.println("Course is Saved: " + course);
-        assertEquals(course.getCourseName(), postResponse.getBody().getCourseName());
-
+        String url = baseURL +"/read";
+        System.out.println("URL: " + url);
+        ResponseEntity<Course> postResponse = restTemplate.getForEntity(url, Course.class);
+        assertNotEquals(course.getCourseName(), postResponse.getBody().getCourseName());
     }
 
-
-
     @Test
-    void update() {
-        Course update = new Course.Builder().copy(course).setCourseName("Human Resources Management").Build();
+    void update(){
+        Course updated = new Course.Builder().copy(course).setCourseName("Multimedia").Build();
         String url = baseURL + "/update";
-        System.out.println("URL: " + url);
-        System.out.println("Post: " + update);
-        ResponseEntity<Course>response = restTemplate.postForEntity(url, update, Course.class);
+        System.out.println("URL:" + url);
+        System.out.println("post data: " + updated);
+        ResponseEntity<Course> response = restTemplate.postForEntity(url, updated, Course.class);
         assertNotNull(response.getBody());
     }
 
     @Test
     void delete() {
-        String url = baseURL + "/delete/" + course.getCourseName();
+        String url = baseURL + "/delete";
         System.out.println("URL: " + url);
         restTemplate.delete(url);
+
     }
 
     @Test
-    void getAll() {
+    void getall(){
         String url = baseURL + "/getall";
         HttpHeaders headers = new HttpHeaders();
         HttpEntity<String> entity = new HttpEntity<>(null, headers);
-        ResponseEntity<String>response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-        System.out.println("List All: ");
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        System.out.println("Show All:");
         System.out.println(response);
         System.out.println(response.getBody());
     }
+
+
 }
--- src/test/java/za/ac/cput/Controller/DepartmentControllerTest.java
+++ src/test/java/za/ac/cput/Controller/DepartmentControllerTest.java
@@ -4,8 +4,11 @@ package za.ac.cput.Controller;
         Author: Devon Daniels (217299822)
 
  */
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -14,18 +17,23 @@ import org.springframework.http.ResponseEntity;
 import za.ac.cput.Entity.Course;
 import za.ac.cput.Entity.Department;
 
+import za.ac.cput.factory.CourseFactory;
 import za.ac.cput.factory.DepartmentFactory;
 import static org.junit.jupiter.api.Assertions.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
 class DepartmentControllerTest {
 
-    private static Department department = DepartmentFactory.buildDepartment("ICT", "Information Technology", "HRM", "Lecturer");
+    private static Department department = DepartmentFactory.buildDepartment("Information ", "TEch", "Science", "lecturer");
     @Autowired
     private TestRestTemplate restTemplate;
-    private final String baseURL = "http://localhost:8080/dept";
+    private final String baseURL = "http://localhost:8080/department";
+
 
     @Test
     void create() {
@@ -34,46 +42,45 @@ class DepartmentControllerTest {
         assertNotNull(postResponse);
         assertNotNull(postResponse.getBody());
         department = postResponse.getBody();
-        System.out.println("Department is Saved: " + department);
+        System.out.println("Saved course: " + department);
         assertEquals(department.getDeptName(), postResponse.getBody().getDeptName());
     }
 
     @Test
     void read() {
-        String url = baseURL + "/create";
-        ResponseEntity<Department> postResponse = restTemplate.postForEntity(url, department, Department.class);
-        assertNotNull(postResponse);
-        assertNotNull(postResponse.getBody());
-        department = postResponse.getBody();
-        System.out.println("Course is Saved: " + department);
-        assertEquals(department.getDeptName(), postResponse.getBody().getDeptName());
-
+        String url = baseURL + "/read";
+        System.out.println("URL: " + url);
+        ResponseEntity<Department> postResponse = restTemplate.getForEntity(url, Department.class);
+        assertNull(department.getDeptName(), postResponse.getBody().getDeptName());
     }
+
     @Test
     void update() {
-        Department update = new Department.Builder().copy(department).setDeptName("Design").Build();
+        Department updated = new Department.Builder().copy(department).setDeptName("Networking").Build();
         String url = baseURL + "/update";
-        System.out.println("URL: " + url);
-        System.out.println("Post: " + update);
-        ResponseEntity<Course>response = restTemplate.postForEntity(url, update, Course.class);
+        System.out.println("URL:" + url);
+        System.out.println("post data: " + updated);
+        ResponseEntity<Department> response = restTemplate.postForEntity(url, updated, Department.class);
         assertNotNull(response.getBody());
     }
 
     @Test
     void delete() {
-        String url = baseURL + "/delete/" + department.getDeptName();
+        String url = baseURL + "/delete";
         System.out.println("URL: " + url);
         restTemplate.delete(url);
+
     }
 
     @Test
-    void getAll() {
+    void getall() {
         String url = baseURL + "/getall";
         HttpHeaders headers = new HttpHeaders();
         HttpEntity<String> entity = new HttpEntity<>(null, headers);
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-        System.out.println("List All: ");
+        System.out.println("Show All:");
         System.out.println(response);
         System.out.println(response.getBody());
     }
-    }
+}
+
